6cb299e7fc89d80b0d9b76fb7dcfa9df1cecf1c1
yssccc <1000yseo@gmail.com>
2025-11-12 23:22:12 +0900

    feat: footer에 Github 아이콘 추가

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,6 +1,24 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function Footer() {
   return (
-    <footer className="h-16 flex items-center justify-center min-h-30">
+    <footer className="flex flex-col items-center justify-center min-h-50 gap-5">
+      <Link
+        href="https://github.com/yssccc"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="cursor-pointer"
+        title="GitHub로 이동"
+      >
+        <Image
+          src="/github-mark.svg"
+          alt="GitHub"
+          width={30}
+          height={30}
+          className="opacity-80 hover:opacity-100 transition"
+        />
+      </Link>
       <div className="text-gray-400 text-center text-sm">
         © 2025 YUNSEO All rights reserved.
       </div>
