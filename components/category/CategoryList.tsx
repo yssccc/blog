60f4fa87fd4892dbd56bf45e9d3ac193185b0629
yssccc <1000yseo@gmail.com>
2025-11-12 21:48:30 +0900

    feat: 카테고리 count로 정렬

--- components/category/CategoryList.tsx
+++ components/category/CategoryList.tsx
@@ -19,12 +19,13 @@ export default function CategoryList({
 }: CategoryListProps) {
   const searchParams = useSearchParams();
   const currentCategory = searchParams.get('category');
+  const sortedCategories = [...categories].sort((a, b) => b.count - a.count);
 
   return (
-    <div className="flex flex-col space-y-1 gap-1 w-30">
+    <div className="flex flex-col gap-1 w-30">
       <Link
         href="/"
-        className={`flex items-center p-2 gap-3 text-sm transition cursor-pointer
+        className={`flex items-center p-1 gap-1 text-sm transition cursor-pointer
           ${!currentCategory ? 'text-main font-semibold' : ''}
         `}
       >
@@ -32,11 +33,11 @@ export default function CategoryList({
         <span className="text-gray-400">({totalPostCount})</span>
       </Link>
 
-      {categories.map((category) => (
+      {sortedCategories.map((category) => (
         <Link
           key={category.name}
           href={`/?category=${encodeURIComponent(category.name)}`}
-          className={`flex items-center p-2 gap-3 text-sm transition cursor-pointer
+          className={`flex items-center p-1 gap-1 text-sm transition cursor-pointer
             ${
               currentCategory === category.name ? 'text-main font-semibold' : ''
             }`}
