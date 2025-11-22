5d1c8588dd45d1fc8a4b90adec04f9938b1ce72d
yssccc <1000yseo@gmail.com>
2025-11-22 22:24:38 +0900

    feat: robots 추가

---
+++ app/robots.ts
@@ -0,0 +1,16 @@
+import { MetadataRoute } from 'next';
+
+const baseUrl = 'https://www.yssccc.com';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/admin/', '/api/'],
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+  };
+}
